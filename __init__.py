25324a8d362a46826f1d507cc1058e60d976d96e
MITHILESH K S <mithidec2006@gmail.com>
2025-09-06 09:43:12 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# Utils package initialization
-# This file makes the utils directory a Python package
-
-from .xss_scanner import XSSScanner
-from .sqli_scanner import SQLInjectionScanner
-from .crawler import WebCrawler
-from .ai_analysis import AIAnalyzer
-
-__all__ = ['XSSScanner', 'SQLInjectionScanner', 'WebCrawler', 'AIAnalyzer']
